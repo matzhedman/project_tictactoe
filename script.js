a00a5a2df7681918e64cbe37ae27c7ac262a2874
matzhedman <hedmanmatz@gmail.com>
2022-12-28 17:15:57 +0000

    test2

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+const scoreBoard = document.getElementById("scoreboard");
+  let score = 0; 
+  let player_x = document.getElementById("player_x");
+  let player_o = document.getElementById("player_o");
+  let tie = document.getElementById("tie");
+
+function x_win() {
+	console.log("incrementing");
+	score++;
+	player_x.innerHTML = score;
+	console.log(player_x);
+}
+
 /* Function to mark tiles with X or O */
 flag = 1;
+
 function mark_1() {
 	if (flag == 1) {
 		document.getElementById("tile1").value = "x";
 		document.getElementById("tile1").disabled = true;
 		flag = 0;
-	}
-	else {
+	} else {
 		document.getElementById("tile1").value = "o";
 		document.getElementById("tile1").disabled = true;
 		flag = 1;
@@ -18,8 +31,7 @@ function mark_2() {
 		document.getElementById("tile2").value = "x";
 		document.getElementById("tile2").disabled = true;
 		flag = 0;
-	}
-	else {
+	} else {
 		document.getElementById("tile2").value = "o";
 		document.getElementById("tile2").disabled = true;
 		flag = 1;
@@ -31,8 +43,7 @@ function mark_3() {
 		document.getElementById("tile3").value = "x";
 		document.getElementById("tile3").disabled = true;
 		flag = 0;
-	}
-	else {
+	} else {
 		document.getElementById("tile3").value = "o";
 		document.getElementById("tile3").disabled = true;
 		flag = 1;
@@ -44,8 +55,7 @@ function mark_4() {
 		document.getElementById("tile4").value = "x";
 		document.getElementById("tile4").disabled = true;
 		flag = 0;
-	}
-	else {
+	} else {
 		document.getElementById("tile4").value = "o";
 		document.getElementById("tile4").disabled = true;
 		flag = 1;
@@ -57,8 +67,7 @@ function mark_5() {
 		document.getElementById("tile5").value = "x";
 		document.getElementById("tile5").disabled = true;
 		flag = 0;
-	}
-	else {
+	} else {
 		document.getElementById("tile5").value = "o";
 		document.getElementById("tile5").disabled = true;
 		flag = 1;
@@ -70,8 +79,7 @@ function mark_6() {
 		document.getElementById("tile6").value = "x";
 		document.getElementById("tile6").disabled = true;
 		flag = 0;
-	}
-	else {
+	} else {
 		document.getElementById("tile6").value = "o";
 		document.getElementById("tile6").disabled = true;
 		flag = 1;
@@ -83,8 +91,7 @@ function mark_7() {
 		document.getElementById("tile7").value = "x";
 		document.getElementById("tile7").disabled = true;
 		flag = 0;
-	}
-	else {
+	} else {
 		document.getElementById("tile7").value = "o";
 		document.getElementById("tile7").disabled = true;
 		flag = 1;
@@ -96,8 +103,7 @@ function mark_8() {
 		document.getElementById("tile8").value = "x";
 		document.getElementById("tile8").disabled = true;
 		flag = 0;
-	}
-	else {
+	} else {
 		document.getElementById("tile8").value = "o";
 		document.getElementById("tile8").disabled = true;
 		flag = 1;
@@ -109,8 +115,7 @@ function mark_9() {
 		document.getElementById("tile9").value = "x";
 		document.getElementById("tile9").disabled = true;
 		flag = 0;
-	}
-	else {
+	} else {
 		document.getElementById("tile9").value = "o";
 		document.getElementById("tile9").disabled = true;
 		flag = 1;
@@ -138,69 +143,86 @@ function reset() {
 	document.getElementById("tile9").value = '';
 	document.getElementById("tile9").disabled = false;
 	flag = 1;
+	document.getElementById('print')
+			.innerHTML = "";
 }
 
 /* whatsNext */
-/* win conditions
-1, 2, 3
-4, 5, 6
-7, 8, 9
-
-1, 4, 7
-2, 5, 8
-3, 6, 9
-
-1, 5, 9
-3, 5, 7 */
 function whatsNext() {
-	var tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9; 
-		tile1 = document.getElementById("tile1").value;
-		tile2 = document.getElementById("tile2").value;
-		tile3 = document.getElementById("tile3").value;
-		tile4 = document.getElementById("tile4").value;
-		tile5 = document.getElementById("tile5").value;
-		tile6 = document.getElementById("tile6").value;
-		tile7 = document.getElementById("tile7").value;
-		tile8 = document.getElementById("tile8").value;
-		tile9 = document.getElementById("tile9").value;
+	var tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9;
+	tile1 = document.getElementById("tile1").value;
+	tile2 = document.getElementById("tile2").value;
+	tile3 = document.getElementById("tile3").value;
+	tile4 = document.getElementById("tile4").value;
+	tile5 = document.getElementById("tile5").value;
+	tile6 = document.getElementById("tile6").value;
+	tile7 = document.getElementById("tile7").value;
+	tile8 = document.getElementById("tile8").value;
+	tile9 = document.getElementById("tile9").value;
 
-/* player X won */
-if (
-  (tile1 == "x" && tile2 == "x" && tile3 == "x") || 
-  (tile4 == "x" && tile5 == "x" && tile6 == "x") ||
-  (tile7 == "x" && tile8 == "x" && tile9 == "x") ||
+	/* win conditions */
+	if (
+		(tile1 == "x" && tile2 == "x" && tile3 == "x") ||
+		(tile4 == "x" && tile5 == "x" && tile6 == "x") ||
+		(tile7 == "x" && tile8 == "x" && tile9 == "x") ||
 
-  (tile1 == "x" && tile4 == "x" && tile7 == "x") || 
-  (tile2 == "x" && tile5 == "x" && tile8 == "x") ||
-  (tile3 == "x" && tile6 == "x" && tile9 == "x") ||
+		(tile1 == "x" && tile4 == "x" && tile7 == "x") ||
+		(tile2 == "x" && tile5 == "x" && tile8 == "x") ||
+		(tile3 == "x" && tile6 == "x" && tile9 == "x") ||
 
-  (tile1 == "x" && tile5 == "x" && tile9 == "x") || 
-  (tile3 == "x" && tile5 == "x" && tile7 == "x")
-)
-document.getElementById('print')
-            .innerHTML = "Player X won";
+		(tile1 == "x" && tile5 == "x" && tile9 == "x") ||
+		(tile3 == "x" && tile5 == "x" && tile7 == "x")
+	) {
+		document.getElementById("tile1").disabled = true;
+		document.getElementById("tile2").disabled = true;
+		document.getElementById("tile3").disabled = true;
+		document.getElementById("tile4").disabled = true;
+		document.getElementById("tile5").disabled = true;
+		document.getElementById("tile6").disabled = true;
+		document.getElementById("tile7").disabled = true;
+		document.getElementById("tile8").disabled = true;
+		document.getElementById("tile9").disabled = true;
 
-else if (
-	(tile1 == "o" && tile2 == "o" && tile3 == "o") || 
-	(tile4 == "o" && tile5 == "o" && tile6 == "o") ||
-	(tile7 == "o" && tile8 == "o" && tile9 == "o") ||
-  
-	(tile1 == "o" && tile4 == "o" && tile7 == "o") || 
-	(tile2 == "o" && tile5 == "o" && tile8 == "o") ||
-	(tile3 == "o" && tile6 == "o" && tile9 == "o") ||
-  
-	(tile1 == "o" && tile5 == "o" && tile9 == "o") || 
-	(tile3 == "o" && tile5 == "o" && tile7 == "o")
-  )
-  document.getElementById('print')
-			  .innerHTML = "Player O won";
-		
+		document.getElementById('print')
+			.innerHTML = "Player X won";
+			x_win();
 			
 
 
+	} else if (
+		(tile1 == "o" && tile2 == "o" && tile3 == "o") ||
+		(tile4 == "o" && tile5 == "o" && tile6 == "o") ||
+		(tile7 == "o" && tile8 == "o" && tile9 == "o") ||
+
+		(tile1 == "o" && tile4 == "o" && tile7 == "o") ||
+		(tile2 == "o" && tile5 == "o" && tile8 == "o") ||
+		(tile3 == "o" && tile6 == "o" && tile9 == "o") ||
+
+		(tile1 == "o" && tile5 == "o" && tile9 == "o") ||
+		(tile3 == "o" && tile5 == "o" && tile7 == "o")
+	)
+
+	{
+		document.getElementById("tile1").disabled = true;
+		document.getElementById("tile2").disabled = true;
+		document.getElementById("tile3").disabled = true;
+		document.getElementById("tile4").disabled = true;
+		document.getElementById("tile5").disabled = true;
+		document.getElementById("tile6").disabled = true;
+		document.getElementById("tile7").disabled = true;
+		document.getElementById("tile8").disabled = true;
+		document.getElementById("tile9").disabled = true;
+		document.getElementById('print')
+			.innerHTML = "Player O won";
+	} else if ((tile1 == "o" || tile1 == "x") && (tile2 == "o" || tile2 == "x") && (tile3 == "o" || tile3 == "x") &&
+		(tile4 == "o" || tile4 == "x") && (tile5 == "o" || tile5 == "x") && (tile6 == "o" || tile6 == "x") &&
+		(tile7 == "o" || tile7 == "x") && (tile8 == "o" || tile8 == "x") && (tile9 == "o" || tile9 == "x"))
+
+	{
+		document.getElementById('print')
+			.innerHTML = "Tie!";
+
+	}
+
+
 }
-
-
-
-
-
